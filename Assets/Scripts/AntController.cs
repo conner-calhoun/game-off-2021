a1f8a765948f828abc0bf05431e267bea113f0b0
conner-calhoun <conner610@gmail.com>
2021-11-07 09:32:31 -0700

    adds ant testing scene w/ player

--- Assets/Scripts/AntController.cs
+++ Assets/Scripts/AntController.cs
@@ -5,30 +5,53 @@ using UnityEngine;
 public class AntController : MonoBehaviour
 {
     public float speed = 1f;
-    public float forwardRay = 2f;
-    public float backwardRay = 3f;
-    public float rayLength = 1.5f; // This will need to be tweaked for larger ants
+    public float rotationSpeed = 5f;
 
+
+
+    // External GameObjects
+    GameObject player;
+
+    // Children & Components
     Rigidbody body;
     Transform rayOrigin;
+    Transform moveToPoint;
+    Transform headTarget;
+
     ContactPoint[] cPoints;
     Vector3 groundNormal;
     Vector3 currentGravity;
     float reNormalTime = 5f;
+    bool isGrounded = true;
+
+    // RayCasting angle modifier
+    float rayLength = 1.5f; // Turns out this matches the scale pretty closely
+    float forwardRay = 2f; // points in front of ant
+    float backwardRay = 3f; // points behind ant
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "AntIgnoreCollision")
+        {
+            Physics.IgnoreCollision(other.gameObject.GetComponent<Collider>(), GetComponent<Collider>());
+        }
+    }
 
     void OnCollisionStay(Collision ourCollision)
     {
-        CheckGrounded(ourCollision);
+        isGrounded = CheckGrounded(ourCollision);
     }
 
-    void CheckGrounded(Collision newCol)
+    bool CheckGrounded(Collision newCol)
     {
         cPoints = new ContactPoint[newCol.contactCount];
         newCol.GetContacts(cPoints);
         foreach (ContactPoint cP in cPoints)
         {
             SetGroundNormal(cP.normal);
+            return true;
         }
+        return false;
     }
 
     void CheckCorners()
@@ -53,10 +76,42 @@ public class AntController : MonoBehaviour
 
     void DoGravity()
     {
-        body.AddForce(-transform.up * Physics.gravity.magnitude, ForceMode.Force);
+        if (isGrounded)
+        {
+            body.AddForce(-transform.up * Physics.gravity.magnitude, ForceMode.Force);
+        }
+        else
+        {
+            // If not on the ground, just fall like normal
+            body.AddForce(-Vector3.up * Physics.gravity.magnitude, ForceMode.Force);
+        }
     }
 
-    void ExecuteMove()
+    void HandleAI()
+    {
+        if (player)
+        {
+            // Handle AI / Place moveToPoint on player // For now, set the moveToPoint location to the player's location
+            moveToPoint.position = player.transform.position;
+            headTarget.position = player.transform.position;
+        }
+
+        float step = speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, moveToPoint.position, step);
+
+        Vector3 dir = (moveToPoint.position - transform.position).normalized;
+        Quaternion rotation = Quaternion.LookRotation(dir);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.fixedDeltaTime * rotationSpeed);
+    }
+
+    void SetGroundNormal(Vector3 normal)
+    {
+        groundNormal = normal;
+        Quaternion tilt = Quaternion.FromToRotation(body.transform.up, groundNormal);
+        body.transform.rotation = Quaternion.Lerp(body.transform.rotation, tilt * body.transform.rotation, reNormalTime * Time.deltaTime);
+    }
+
+    void ManualInput()
     {
         // Movement will instead be a "MoveTo" point
         if (body.velocity.magnitude < speed)
@@ -72,30 +127,30 @@ public class AntController : MonoBehaviour
         body.transform.Rotate(new Vector3(0f, Input.GetAxis("Horizontal") * 150f * Time.fixedDeltaTime, 0f));
     }
 
-    void SetGroundNormal(Vector3 normal)
-    {
-        groundNormal = normal;
-        Quaternion tilt = Quaternion.FromToRotation(body.transform.up, groundNormal);
-        body.transform.rotation = Quaternion.Lerp(body.transform.rotation, tilt * body.transform.rotation, reNormalTime * Time.deltaTime);
-    }
-
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody>();
         rayOrigin = gameObject.transform.Find("RayOrigin");
+
+        moveToPoint = gameObject.transform.Find("MoveTarget");
+        /// we need to detach the MoveTarget from the ant or it will move with the ant.
+        moveToPoint.parent = null;
+
+        headTarget = gameObject.transform.Find("BugRig/HeadAim/HeadLookAt");
+
         body.useGravity = false;
+
+        rayLength = transform.localScale.x; // Set length of forward raycast to the scale of the ant
+
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
-        // Handle custom gravity (sticking to walls)
-        DoGravity();
-
-        ExecuteMove();
-
         CheckCorners();
+        DoGravity();
+        HandleAI();
     }
 }
